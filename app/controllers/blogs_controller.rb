b12a6f7c891dc909cf80bb694482f2ea0f8cbca5
Uthaeus <Uth@github.com>
2018-01-07 14:52:59 -0700

    refactored blogs controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,5 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show]
 
   def index
   end
@@ -8,15 +9,28 @@ class BlogsController < ApplicationController
   end
 
   def create
-    @blog = Blog.new(params.require(:blog).permit(:title, :date, :body))
+    @blog = Blog.new(blog_params)
 
-    @blog.save
-
-    redirect_to @blog
+    if @blog.save
+      redirect_to @blog, notice: 'Your blog was created successfully'
+    else
+      render :new
+    end
   end
 
-  def show
-    @blog = Blog.find(params[:id])
+  def show 
   end
+
+
+
+  private
+
+    def blog_params
+      params.require(:blog).permit(:title, :date, :body)
+    end
+
+    def set_blog
+      @blog = Blog.find(params[:id])
+    end
 
 end
