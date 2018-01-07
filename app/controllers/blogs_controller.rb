76a2c9666e15d0c804a273dc9f4a14c1e55db521
Uthaeus <Uth@github.com>
2018-01-07 14:46:15 -0700

    Created new and show for blogs using testing

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,5 +2,21 @@ class BlogsController < ApplicationController
 
   def index
   end
-  
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(params.require(:blog).permit(:title, :date, :body))
+
+    @blog.save
+
+    redirect_to @blog
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
 end
--- spec/features/blog_spec.rb
+++ spec/features/blog_spec.rb
@@ -13,4 +13,23 @@ describe 'navigate' do
       expect(page).to have_content(/Blogs/)
     end
   end
+
+  describe 'creation' do
+    before do
+      visit new_blog_path
+    end
+
+    it 'has a new form that can be reached' do
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from new form page' do
+      fill_in 'blog[title]', with: "A title"
+      fill_in 'blog[date]', with: Date.today
+      fill_in 'blog[body]', with: "Something here"
+      click_on "Save"
+
+      expect(page).to have_content("Something here") 
+    end
+  end
 end
